a76fe5c2395a2cfd6f3c6e2805a91ffc168fe995
company-laptop <1649501854@qq.com>
2021-08-31 17:12:09 +0800

    03 string builder test

---
+++ 03-StringBuilderTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Meow");
+            sb.Append(21);
+            sb.Append(true);
+            for (int i = 0; i < 100; i++)
+            {
+                sb.Append(i);
+            }
+            Console.WriteLine(sb.ToString());
+
+            sb.Clear();
+            Console.WriteLine(sb.ToString());
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+    }
+}
---
+++ 03-StringBuilderTest/TimeTest/TimeTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace TimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            string str = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                str += i;
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            sw.Reset();
+            sw.Start();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 1000000; i++)
+            {
+                sb.Append(i);
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+    }
+}
