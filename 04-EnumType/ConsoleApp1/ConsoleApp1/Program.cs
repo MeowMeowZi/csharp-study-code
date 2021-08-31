ef796f1f44975d647c62618f1f2e3e5ab28cc3b5
company-laptop <1649501854@qq.com>
2021-08-31 17:21:43 +0800

    04 enum type

---
+++ 04-EnumType/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public enum Week
+    {
+        星期一,
+        星期二,
+        星期三,
+        星期四,
+        星期五,
+        星期六,
+        星期天
+    }
+
+    public enum Gender
+    {
+        boy,
+        girl
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week day = Week.星期二;
+            Week day_2 = Week.星期四;
+            Console.WriteLine(day);
+
+            Gender p1 = Gender.boy;
+            Gender p2 = Gender.girl;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
