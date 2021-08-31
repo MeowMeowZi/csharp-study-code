87848047cc3a571d1625a50ac7e3e97ab0e4d6ea
company-laptop <1649501854@qq.com>
2021-08-31 14:57:16 +0800

    01-string-test

---
+++ 01-StringTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Meow";
+            Console.WriteLine("ToUpper: " + name.ToUpper());
+            Console.WriteLine("ToLower: " + name.ToLower());
+
+            string name_2 = "lfz";
+            Console.WriteLine("Equals: " + name.Equals(name_2));
+
+            string address = "中国|浙江|杭州";
+            char[] c = new char[] { '|' };
+            string[] addr = address.Split(c);
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine(addr[i]);
+            }
+
+            Console.WriteLine("Substring: " + name.Substring(2));
+            Console.WriteLine("Substring: " + name.Substring(2, 1));
+
+            string name_3 = "MeMwM";
+            Console.WriteLine("IndexOf: " + name_3.IndexOf("M"));
+            Console.WriteLine("IndexOf: " + name_3.IndexOf("m"));
+            Console.WriteLine("LastIndexOf: " + name_3.LastIndexOf("M"));
+            Console.WriteLine("LastIndexOf: " + name_3.LastIndexOf("m"));
+
+            Console.WriteLine("StartsWith: " + name.StartsWith("Me"));
+            Console.WriteLine("StartsWith: " + name.StartsWith("me"));
+            Console.WriteLine("EndsWith: " + name.EndsWith("ow"));
+            Console.WriteLine("EndsWith: " + name.EndsWith("oW"));
+
+            Console.WriteLine("Replace: " + name.Replace("M", "m"));
+        }
+    }
+}
