159590ec3b91f601fddffcc0781d1ac2186c19b0
company-laptop <1649501854@qq.com>
2021-09-01 19:22:51 +0800

    11 extend

---
+++ 11-Extend/ConsoleApp1/ConsoleApp1/Hero.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Hero
+    {
+        private string nickName;
+        private string heroName;
+        private string heroInfo;
+        private float attack;
+        private float defense;
+        private float speed;
+
+        public string NickName
+        {
+            get { return nickName; }
+            set { nickName = value; }
+        }
+        public string HeroName
+        {
+            get { return heroName; }
+            set { heroName = value; }
+        }
+        public string HeroInfo
+        {
+            get { return heroInfo; }
+            set { heroInfo = value; }
+        }
+        public float Attack
+        {
+            get { return attack; }
+            set { attack = value;  }
+        }
+        public float Defense
+        {
+            get { return defense; }
+            set { defense = value; }
+        }
+        public float Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public void Skll1()
+        {
+            Console.WriteLine("Skill1");
+        }
+        public void Skll2()
+        {
+            Console.WriteLine("Skill2");
+        }
+        public void Skll3()
+        {
+            Console.WriteLine("Skill3");
+        }
+    }
+}
