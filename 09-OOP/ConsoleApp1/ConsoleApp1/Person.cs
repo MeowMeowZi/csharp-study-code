96a1dd5a12e46f308207f3559b75361f79809b83
company-laptop <1649501854@qq.com>
2021-09-01 15:44:27 +0800

    09 OOP

---
+++ 09-OOP/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public enum Gender
+    {
+        boy,
+        girl
+    }
+
+    /// <summary>
+    /// Person类，描述人
+    /// </summary>
+    class Person
+    {
+        private string name;
+        private int age;
+        private Gender gender;
+        private string address;
+
+        public Person() { }
+
+        public Person(string name, int age, Gender gender, string address)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.address = address;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    age = -1;
+                }
+            }
+        }        
+
+        public Gender Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }        
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine(Name + " can eat");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(Name + " can sleep");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine(Name + " can work");
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("{0},{1},{2},{3}", Name, Age, Gender, Address);
+        }
+    }
+}
---
+++ 09-OOP/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Apple a1 = new Apple();
+            a1.color = Color.red;
+            a1.weigth = 200;
+            a1.shape = "circle";
+            Console.WriteLine("{0},{1},{2}", a1.color, a1.weigth, a1.shape);
+
+            Person p1 = new Person();
+            p1.Name = "Meow";
+            p1.Age = 21;
+            p1.Gender = Gender.boy;
+            p1.Address = "ShangHai";
+            Console.WriteLine("{0},{1},{2},{3}", p1.Name, p1.Age, p1.Gender, p1.Address);
+            p1.Eat();
+            p1.Sleep();
+            p1.Work();
+
+            Person p2 = new Person("lfz", 21, Gender.girl, "BengBu");
+            Console.WriteLine("{0},{1},{2},{3}", p2.Name, p2.Age, p2.Gender, p2.Address);*/
+
+            Person p1 = new Person("Meow", 17, Gender.girl, "ShangHai");
+            Person p2 = p1;
+            p1.Hello();
+            p2.Hello();
+
+            p2.Name = "lfz";
+            p1.Hello();
+            p2.Hello();
+
+            p1.Age = 21;
+
+            p1.Hello();
+            p2.Hello();
+        }
+    }
+}
