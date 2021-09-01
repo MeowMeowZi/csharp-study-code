7e1d9824d2d410ebe34fb38317d8e2c762eec1c5
company-laptop <1649501854@qq.com>
2021-09-01 14:37:11 +0800

    08 OOP

---
+++ 08-OOP/ConsoleApp1/ConsoleApp1/Apple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public enum Color
+    {
+        red,
+        yellow,
+        bule
+    }
+
+    class Apple
+    {
+        public Color color;
+        public int weigth;
+        public string shape;
+    }
+}
