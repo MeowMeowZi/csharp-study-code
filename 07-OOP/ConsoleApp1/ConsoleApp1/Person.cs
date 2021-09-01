2036489777c2c2580f7eb34158da80df1409a207
company-laptop <1649501854@qq.com>
2021-09-01 14:11:24 +0800

    07 OOP

---
+++ 07-OOP/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public enum Gender
+    {
+        boy,
+        girl
+    }
+
+    /// <summary>
+    /// Person类，描述人
+    /// </summary>
+    class Person
+    {
+        private string name;
+        private int age;
+        private Gender gender;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    age = -1;
+                }
+            }
+        }        
+
+        public Gender Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }        
+        
+        /*public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }*/
+    }
+}
---
+++ 07-OOP/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple a1 = new Apple();
+            a1.color = Color.red;
+            a1.weigth = 200;
+            a1.shape = "circle";
+            Console.WriteLine("{0},{1},{2}", a1.color, a1.weigth, a1.shape);
+
+            Person p1 = new Person();
+            p1.Name = "Meow";
+            p1.Age = 21;
+            p1.Gender = Gender.boy;
+            Console.WriteLine("{0},{1},{2}", p1.Name, p1.Age, p1.Gender);
+        }
+    }
+}
