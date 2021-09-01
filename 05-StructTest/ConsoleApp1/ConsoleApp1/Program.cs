2c7a8293364bf625fd79378e2087702ddff86713
company-laptop <1649501854@qq.com>
2021-09-01 08:56:15 +0800

    05 struct test

---
+++ 05-StructTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public enum Gender
+    {
+        boy,
+        gril
+    }
+
+    public struct Person
+    {
+        public string name;
+        public int age;
+        public Gender gender;
+        public string address;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.name = "Meow";
+            p1.gender = Gender.boy;
+            p1.age = 21;
+            p1.address = "杭州";
+
+            Console.WriteLine(p1.name + p1.gender + p1.age + p1.address);
+        }
+    }
+}
