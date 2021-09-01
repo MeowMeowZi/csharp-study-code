24efcfa6ebb5b73a09038396cbdc1c042a2d6e95
company-laptop <1649501854@qq.com>
2021-09-01 09:28:14 +0800

    06 OPP

---
+++ 06-OOP/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple a1 = new Apple();
+
+            a1.color = Color.red;
+            a1.weigth = 200;
+            a1.shape = "circle";
+
+            Console.WriteLine("{0},{1},{2}", a1.color, a1.weigth, a1.shape);
+        }
+    }
+}
