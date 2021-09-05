c025f1dcac689895c4be4709e8b0a46af5a642d4
MeowMeowZi <1649501854@qq.com>
2021-09-06 01:07:33 +0800

    12 extend

---
+++ 12-Extend/ConsoleApp1/ConsoleApp1/LuKaSi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class LuKaSi : Hero
+    {
+        public LuKaSi() { }
+        public LuKaSi(string m_nickName, string m_heroName, string m_heroInfo, float m_attack, float m_defense, float m_speed)
+            : base(m_nickName, m_heroName, m_heroInfo, m_attack, m_defense, m_speed)
+        {
+
+        }
+    }
+}
