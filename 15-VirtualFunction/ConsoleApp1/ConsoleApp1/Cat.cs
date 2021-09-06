c0e15c341293e8a5ece09e2dd5188af45e2af68a
MeowMeowZi <1649501854@qq.com>
2021-09-06 17:20:55 +0800

    15 virtual function

---
+++ 15-VirtualFunction/ConsoleApp1/ConsoleApp1/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Cat:CatType
+    {
+        public override void Cry()
+        {
+            base.Cry();
+            Console.WriteLine("cat cry");
+        }
+    }
+}
---
+++ 15-VirtualFunction/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiger t = new Tiger();
+            t.Cry();
+
+            Cat c = new Cat();
+            c.Cry();
+        }
+    }
+}
