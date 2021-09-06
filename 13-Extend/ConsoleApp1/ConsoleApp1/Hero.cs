ba22efc226519c972d4b4c927bf5860c39667198
MeowMeowZi <1649501854@qq.com>
2021-09-06 15:56:47 +0800

    13 extend

---
+++ 13-Extend/ConsoleApp1/ConsoleApp1/Hero.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Hero
+    {
+        private string nickName;
+        private string heroName;
+        private string heroInfo;
+        private float attack;
+        private float defense;
+        private float speed;
+        public string test;
+
+        public Hero() { }
+        public Hero(string m_nickName, string m_heroName, string m_heroInfo, float m_attack, float m_defense, float m_speed)
+        {
+            this.nickName = m_nickName;
+            this.heroName = m_heroName;
+            this.heroInfo = m_heroInfo;
+            this.attack = m_attack;
+            this.defense = m_defense;
+            this.speed = m_speed;
+        }
+
+        public string NickName
+        {
+            get { return nickName; }
+            set { nickName = value; }
+        }
+        public string HeroName
+        {
+            get { return heroName; }
+            set { heroName = value; }
+        }
+        public string HeroInfo
+        {
+            get { return heroInfo; }
+            set { heroInfo = value; }
+        }
+        public float Attack
+        {
+            get { return attack; }
+            set { attack = value;  }
+        }
+        public float Defense
+        {
+            get { return defense; }
+            set { defense = value; }
+        }
+        public float Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public void Skll1()
+        {
+            Console.WriteLine("Skill1");
+        }
+        public void Skll2()
+        {
+            Console.WriteLine("Skill2");
+        }
+        public void Skll3()
+        {
+            Console.WriteLine("Skill3");
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("{0},{1},{2},{3},{4},{5}", NickName, HeroName, HeroInfo, Attack, Defense, Speed);
+        }
+    }
+}
---
+++ 13-Extend/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaiLa h1 = new TaiLa("h1", "TaiLa", "hello", 80, 80, 50);
+            AiLuoKe h2 = new AiLuoKe("h2", "AiLuoKe", "hello", 70, 70, 40);
+            LuKaSi h3 = new LuKaSi("h3", "LuKaSi", "hello", 50, 60, 70);
+            LuoFei h4 = new LuoFei("h4", "LuoFei", "hello", 10, 20, 30);
+
+            h1.Skll1();
+            h2.Skll2();
+            h3.Skll3();
+            h1.Hello();
+            h2.Hello();
+            h3.Hello();
+            h4.Hello();
+        }
+    }
+}
