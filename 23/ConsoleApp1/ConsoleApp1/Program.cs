a16e5b48cae89c67376bee7f079ad949c5565746
MeowMeowZi <1649501854@qq.com>
2021-09-22 15:52:22 +0800

    23

---
+++ 23/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new Meow();
+            m.Name = "MeowMeowZi";
+            Console.WriteLine(m.Name);
+        }
+    }
+}
