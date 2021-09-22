e7bac34364f00e8aac03026ff95b942dcb93c18a
MeowMeowZi <1649501854@qq.com>
2021-09-22 16:56:56 +0800

    24

---
+++ 24/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string name;
+        public static string address;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public static string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+         
+    }
+}
---
+++ 24/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.address = "ShangHai";
+            
+            var p1 = new Person();
+            p1.name = "Meow";
+            Console.WriteLine(p1.name + "--" + Person.address + "--" + p1.Name + "--" + Person.Address);
+
+            var p2 = new Person();
+            p2.name = "MeowMeowZi";
+            Console.WriteLine(p2.name + "--" + Person.address + "--" + p2.Name + "--" + Person.Address);
+        }
+    }
+}
