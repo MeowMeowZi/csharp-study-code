a511cada705d71d9452b35362481fe83f6dad87a
MeowMeowZi <1649501854@qq.com>
2021-09-22 15:52:48 +0800

    23

---
+++ 23/ConsoleApp1/ConsoleApp1/Meow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Meow : Person
+    {
+        public void World()
+        {
+            Console.WriteLine(base.Name);
+            Console.WriteLine(base.name);
+        }
+    }
+}
---
+++ 23/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        protected string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("我是父类中的Hello方法");
+        }
+        
+    }
+}
