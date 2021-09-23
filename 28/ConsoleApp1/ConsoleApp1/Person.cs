50207e3f7ee2b26d9015b7fd79d2fc5c4691d866
MeowMeowZi <1649501854@qq.com>
2021-09-23 15:30:44 +0800

    28

---
+++ 28/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private string address;
+
+        public Person(string name, int age, string address)
+        {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine($"{name}--{age}--{address}");
+        }
+
+        public override string ToString()
+        {
+            return $"{name}--{age}--{address}";
+        }
+    }
+}
---
+++ 28/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("Meow", 17, "ShangHai");
+            p.Hello();
+        
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p.GetType());
+            Console.WriteLine(p.GetType().ToString());
+
+            Object m1;
+            object m2;
+
+            int a = 10;
+            Console.WriteLine(a);
+            object b = a;           // 装箱 -- 值类型->引用类型
+            Console.WriteLine(b);
+            a = (int) b;            // 拆箱 -- 引用类型->值类型
+            Console.WriteLine(a);
+            
+        }
+    }
+}
