09a065b9424dd00958a5c284bb864df5a2fc34ba
MeowMeowZi <1649501854@qq.com>
2021-09-23 15:42:18 +0800

    29

---
+++ 29/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        #region 字段
+
+        private string name;
+        private int age;
+        private string address;
+
+        #endregion
+
+        #region 属性
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        #endregion
+
+        #region 构造方法
+
+        // public Person(string name, int age, string address)
+        // {
+        //     this.name = name;
+        //     this.age = age;
+        //     this.address = address;
+        // }
+
+        #endregion
+
+        #region 普通方法
+
+        public void Hello()
+        {
+            Console.WriteLine($"{name}--{age}--{address}");
+        }
+        
+        public override string ToString()
+        {
+            return $"{name}--{age}--{address}";
+        }
+
+        #endregion
+
+    }
+}
---
+++ 29/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person()
+            {
+                Name = "Meow",
+                Age = 17,
+                Address = "ShangHai"
+            };
+            p.Hello();
+        
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p.GetType());
+            Console.WriteLine(p.GetType().ToString());
+
+            Object m1;
+            object m2;
+
+            int a = 10;
+            Console.WriteLine(a);
+            object b = a;           // 装箱 -- 值类型->引用类型
+            Console.WriteLine(b);
+            a = (int) b;            // 拆箱 -- 引用类型->值类型
+            Console.WriteLine(a);
+            
+        }
+    }
+}
