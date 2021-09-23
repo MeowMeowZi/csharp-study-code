9d5f20741e797b9d9ddad8ad3ff4bca3f879c847
MeowMeowZi <1649501854@qq.com>
2021-09-23 15:05:49 +0800

    27

---
+++ 27/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("hello");
+        }
+
+        public class Web
+        {
+            public string webName;
+            public string webUrl;
+
+            public void Show()
+            {
+                Console.WriteLine("{0}:{1}", webName, webUrl);
+            }
+        }
+    }
+}
---
+++ 27/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Hello();
+
+            var w = new Person.Web
+            {
+                webName = "BiliBili",
+                webUrl = "www.bilibili.com"
+            };
+            w.Show();
+
+            var m = new {Name = "Meow", Age = 17};
+            Console.WriteLine("{0}:{1}", m.Name, m.Age);
+        }
+    }
+}
