6a47ba2c4583439fc1e57305f2c86ab23ca55958
MeowMeowZi <1649501854@qq.com>
2021-09-27 11:41:35 +0800

    31

---
+++ 31/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weapons = new List<Weapon>();
+
+            var item = new Weapon(1, "霸王枪", 100);
+            weapons.Add(item);
+
+            weapons.Add(new Weapon(2, "长剑", 60));
+            weapons.Add(new Weapon(3, "匕首", 40));
+            weapons.Add(new Weapon(4, "盾牌", 0));
+            foreach (var iWeapon in weapons)
+            {
+                Console.WriteLine(iWeapon);
+            }
+
+            Console.WriteLine("--------------------");
+            weapons.RemoveAt(1);
+            foreach (var iWeapon in weapons)
+            {
+                Console.WriteLine(iWeapon);
+            }
+            
+            
+            
+            Console.WriteLine("--------------------");
+            foreach (var iWeapon in weapons)
+            {
+                if (iWeapon.Name == "盾牌")
+                {
+                    weapons.Remove(iWeapon);
+                    break;
+                }
+            }
+            foreach (var iWeapon in weapons)
+            {
+                Console.WriteLine(iWeapon);
+            }
+            
+            Console.WriteLine("--------------------");
+            foreach (var iWeapon in weapons)
+            {
+                if (iWeapon.Name == "匕首")
+                {
+                    iWeapon.Attack = 99999;
+                    // break;
+                }
+            }
+            foreach (var iWeapon in weapons)
+            {
+                Console.WriteLine(iWeapon);
+            }
+        }
+    }
+}
---
+++ 31/ConsoleApp1/ConsoleApp1/Weapon.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    public class Weapon
+    {
+        private int id;
+        private string name;
+        private int attack;
+        
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Attack
+        {
+            get { return attack; }
+            set { attack = value; }
+        }
+
+        public Weapon(int id, string name, int attack)
+        {
+            this.id = id;
+            this.name = name;
+            this.attack = attack;
+        }
+        
+        public override string ToString()
+        {
+            return $"id:{id} - name:{name} - attack:{attack}";
+        }
+    }
+}
